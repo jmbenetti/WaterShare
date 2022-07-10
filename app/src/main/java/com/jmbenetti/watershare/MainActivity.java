fd1183bfc1723727074900e0a36e59949d4777a3
José Manuel Benetti <josemanuelbenetti@gmail.com>
2022-07-10 17:36:09 -0300

    Resizable watermark

--- app/src/main/java/com/jmbenetti/watershare/MainActivity.java
+++ app/src/main/java/com/jmbenetti/watershare/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Matrix;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
@@ -25,12 +26,20 @@ public class MainActivity extends AppCompatActivity {
 
     Button share;
     ImageView imageView;
+    double nEscala = 1;
+    double nMinimo = 0.1;
+    double nMaximo = 10;
+    double nVariacion = 0.1;
+    Button btnAumentar;
+    Button btnReducir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         share = findViewById(R.id.share);
+        btnAumentar = findViewById(R.id.btnAumentar);
+        btnReducir = findViewById(R.id.btnReducir);
         imageView = findViewById(R.id.shareimage);
 
         RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
@@ -39,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
         );
         imageView.setLayoutParams(params);
 
-        agregarMarca();
+        dibujarConMarca();
 
         share.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,21 +58,54 @@ public class MainActivity extends AppCompatActivity {
                 shareImageandText(bitmap);
             }
         });
+
+        btnAumentar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(getApplicationContext(),"Aumentar",Toast.LENGTH_SHORT).show();
+                if (nEscala + nVariacion <= nMaximo) {
+                    nEscala += nVariacion;
+                    dibujarConMarca();
+                }
+            }
+        });
+
+        btnReducir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(getApplicationContext(),"Reducir",Toast.LENGTH_SHORT).show();
+                if (nEscala - nVariacion >= nMinimo) {
+                    nEscala -= nVariacion;
+                    dibujarConMarca();
+                }
+            }
+        });
+
     }
 
 
-    private void agregarMarca() {
+    private void dibujarConMarca() {
         Resources res = getResources();
         Drawable drawableMarca = ResourcesCompat.getDrawable(res, R.drawable.marcadeaguaoscura, null);
 
-        // The bitmap we want to watermark. It must be mutable.
-        Bitmap bmpOriginal = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
+        // El drawable original
+        Drawable drawableImagen = ResourcesCompat.getDrawable(res, R.drawable.soniquito, null);
+        // Lo convierto en bitmap
+        Bitmap bmpOriginal = ((BitmapDrawable) drawableImagen).getBitmap();
+
+        // Lo convierto en mutable
         Bitmap mainBitmap = bmpOriginal.copy(Bitmap.Config.ARGB_8888, true);
-        // The watermark itself. It doesn't have to be mutable.
-        Bitmap watermarkBitmap = ((BitmapDrawable)drawableMarca).getBitmap();
-        // Creating a canvas with mainBitmap
+        // La marca de agua
+        Bitmap watermarkOriginal = ((BitmapDrawable) drawableMarca).getBitmap();
+        double nAlto = watermarkOriginal.getHeight();
+        double nAncho = watermarkOriginal.getWidth();
+
+        // La marca de agua cambiada de tamaño
+        Bitmap watermarkBitmap = getResizedBitmap(watermarkOriginal, (int) (nAncho * nEscala),
+                (int) (nAlto * nEscala));
+        // Creo un canvas con el bitmap mutable
         Canvas canvas = new Canvas(mainBitmap);
-        // The actual watermarking
+        // Pongo la marca de agua
         canvas.drawBitmap(watermarkBitmap, 0, 0, null);
         imageView.setImageDrawable(new BitmapDrawable(getResources(), mainBitmap));
     }
@@ -105,4 +147,19 @@ public class MainActivity extends AppCompatActivity {
         }
         return uri;
     }
+
+    public static Bitmap getResizedBitmap(Bitmap bm, int newWidth, int newHeight) {
+        int width = bm.getWidth();
+        int height = bm.getHeight();
+        float scaleWidth = ((float) newWidth) / width;
+        float scaleHeight = ((float) newHeight) / height;
+        // Crear matrix para manipulación
+        Matrix matrix = new Matrix();
+        // Cambiar tamaño de bitmap
+        matrix.postScale(scaleWidth, scaleHeight);
+        // Recrear el bitmap
+        return Bitmap.createBitmap(
+                bm, 0, 0, width, height, matrix, false);
+    }
+
 }
